7c912a6fdc4b7384122794c3f9a529f5dd7cc438
Jeffrey Pfau <jeffrey@endrift.com>
2015-01-12 23:01:08 -0800

    Sample sensor from WarioWare Twisted!

--- source/main.c
+++ source/main.c
@@ -2,9 +2,16 @@
 #include <stdio.h>
 
 vu8* const SENSOR_BASE = SRAM + 0x8000;
+vu16* const GPIO_DATA = (vu16*) 0x080000C4;
+vu16* const GPIO_DIR = (vu16*) 0x080000C6;
+vu16* const GPIO_CNT = (vu16*) 0x080000C8;
 
 enum Sensor {
-	SENSOR_TILT = 1
+	SENSOR_TILT = 1,
+	SENSOR_GYRO = 2,
+	SENSOR_LIGHT = 4,
+	SENSOR_RTC = 8,
+	SENSOR_RUMBLE = 16
 };
 
 const struct GameData {
@@ -13,6 +20,7 @@ const struct GameData {
 	int sensors;
 } data[] = {
 	{ "KYGE", "Yoshi's Topsy-Turvy", SENSOR_TILT },
+	{ "RZWE", "WarioWare Twisted!", SENSOR_GYRO | RUMBLE },
 	{ 0, 0, 0 }
 };
 
@@ -26,12 +34,42 @@ const struct GameData* detectGame(void) {
 	return 0;
 }
 
+void setupGPIO(int pins) {
+	*GPIO_CNT = 1;
+	*GPIO_DIR = pins & 0xF;
+}
+
+void setupGyro(void) {
+	setupGPIO(0xB);
+	u16 exmem = REG_EXMEMCNT;
+	exmem &= ~0x7F;
+	exmem |= 0x17;
+	REG_EXMEMCNT = exmem;
+	printf("Preparing cartridge, setting to %04X\n", exmem);
+}
+
+void testGyro(void) {
+	u16 state = *GPIO_DATA & 0x8;
+	*GPIO_DATA = state | 3;
+	*GPIO_DATA = state | 2;
+	int i;
+	u16 data = 0;
+	for (i = 0; i < 16; ++i) {
+		u16 bit = (*GPIO_DATA & 4) >> 2;
+		*GPIO_DATA = state;
+		data |= bit << (15 - i);
+		*GPIO_DATA = state | 2;
+	}
+	data &= 0xFFF;
+	printf("Gyro z: %03X\n", data);
+}
+
 void setupTilt(void) {
 	u16 exmem = REG_EXMEMCNT;
 	exmem &= ~0x63;
 	exmem |= 0x23;
 	REG_EXMEMCNT = exmem;
-	printf("Preparing cartridge, setting to %04X", exmem);
+	printf("Preparing cartridge, setting to %04X\n", exmem);
 }
 
 void testTilt(void) {
@@ -60,12 +98,22 @@ int main(void) {
 		if (game->sensors & SENSOR_TILT) {
 			setupTilt();
 		}
+		if (game->sensors & SENSOR_GYRO) {
+			setupGyro();
+		}
 	}
-	while(1){
-		if (game && game->sensors & SENSOR_TILT) {
+	while (1) {
+		swiWaitForVBlank();
+		if (!game) {
+			continue;
+		}
+		if (game->sensors & SENSOR_TILT) {
 			testTilt();
 		}
-		swiWaitForVBlank();
+		if (game->sensors & SENSOR_GYRO) {
+			testGyro();
+		}
+
 	}
 
 	return 0;
